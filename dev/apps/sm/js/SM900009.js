3287a4fbe57fc4f80b6cd7c9e37b7cd9bb567cec
xinternet <xinternet00@gmail.com>
2016-11-04 10:21:49 +0900

    가이드 문서 프로그램 수정

--- dev/apps/sm/js/SM900009.js
+++ dev/apps/sm/js/SM900009.js
@@ -1,3 +1,13 @@
 $(function() {
+  setTab();
+
   _X.UnBlock();
 });
+
+var setTab = function() {
+  $("#tab-guide").append('<li role="presentation" ><a href="#grid-js" id="tab-grid-js" aria-controls="grid-js" role="tab" data-toggle="tab">Grid.js</a></li>');
+
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    alert($(this).attr("id"));
+  });
+}
